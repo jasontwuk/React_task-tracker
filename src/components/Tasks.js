817a3c7e1a7c556a381e497c46c34f408674ef56
Jason <jasontwuk@gmail.com>
2021-03-21 21:02:06 +0000

    change argument name

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -2,7 +2,7 @@ import Task from "./Task";
 import { useTransition, animated } from "react-spring";
 
 const Tasks = ({ tasks, deleteTask, editTask }) => {
-  const transition = useTransition(tasks, (tasks) => tasks.id, {
+  const transition = useTransition(tasks, (task) => task.id, {
     from: { opacity: 0, marginLeft: -100, marginRight: 100 },
     enter: { opacity: 1, marginLeft: 0, marginRight: 0 },
     leave: { opacity: 0, marginLeft: 100, marginRight: -100 },
