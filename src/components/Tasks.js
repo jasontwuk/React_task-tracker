57df13006a4123aeb7241934ef4e3767f036d641
Jason <jasontwuk@gmail.com>
2021-03-23 09:01:13 +0000

    adjust useTransition argument value

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -3,9 +3,10 @@ import { useTransition, animated } from "react-spring";
 
 const Tasks = ({ tasks, deleteTask, editTask }) => {
   const transition = useTransition(tasks, (task) => task.id, {
-    from: { opacity: 0, marginLeft: -100, marginRight: 100 },
+    from: { opacity: 0, marginLeft: -200, marginRight: 200 },
     enter: { opacity: 1, marginLeft: 0, marginRight: 0 },
-    leave: { opacity: 0, marginLeft: 100, marginRight: -100 },
+    leave: { opacity: 0, marginLeft: 300, marginRight: -300 },
+    config: { duration: 250 },
   });
 
   return (
