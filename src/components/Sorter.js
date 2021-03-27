11e1a6d2539557749cad8be1ae81141abc60c531
Jason <jasontwuk@gmail.com>
2021-03-27 18:28:50 +0000

    build Sorter.js & _sorter.scss

---
+++ src/components/Sorter.js
@@ -0,0 +1,28 @@
+const Sorter = () => {
+  return (
+    <div className="sorter">
+      <ul>
+        <li>
+          <button className="all">all</button>
+        </li>
+        <li>
+          <button className="blue">&nbsp;</button>
+        </li>
+        <li>
+          <button className="pink">&nbsp;</button>
+        </li>
+        <li>
+          <button className="yellow">&nbsp;</button>
+        </li>
+        <li>
+          <button className="green">&nbsp;</button>
+        </li>
+        <li>
+          <button className="purple">&nbsp;</button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sorter;
